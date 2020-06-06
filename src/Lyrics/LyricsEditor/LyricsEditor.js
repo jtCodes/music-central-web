b3d8350b05b47dc1deebc6e1ae4f9dc630455888
jtCodes <jt234wq@gmail.com>
2020-06-06 12:58:12 -0400

    player working

---
+++ src/Lyrics/LyricsEditor/LyricsEditor.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import "./LyricsEditor.css"
+
+class LyricsEditor extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+                 
+        }
+    }
+
+    render() {
+        return (
+            <div className='lyrcis-editor ms-100vw ms-100vh'>
+                
+            </div>
+        )
+    }
+}
+
+export default LyricsEditor
